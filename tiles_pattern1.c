237416a93517876ded48dcb89e1b03fc28b60a62
karelumair <umair.karel@gmail.com>
2024-05-21 11:17:45 +0530

    refactor: fixed spaces

--- tiles_pattern1.c
+++ tiles_pattern1.c
@@ -4,28 +4,28 @@ int i = 0;
 
 void print1(void)
 {
-	
-	for(int j = 9; j >= i; j--){
-		if(j == i)
-			printf("* ");
-		else
-			printf(" ");
-	}
+    
+    for(int j = 9; j >= i; j--){
+        if(j == i)
+            printf("* ");
+        else
+            printf(" ");
+    }
 
-	for(int k = i; k > 0; k--){
-		printf(" ");
-	}
+    for(int k = i; k > 0; k--){
+        printf(" ");
+    }
 
-	for(int k = 0; k <= i; k++){
-		if(k == i)
-			printf("* ");
-		else
-			printf(" ");
-	}
+    for(int k = 0; k <= i; k++){
+        if(k == i)
+            printf("* ");
+        else
+            printf(" ");
+    }
 
-	for(int j = 9; j > i; j--){
-		printf(" ");
-	}
+    for(int j = 9; j > i; j--){
+        printf(" ");
+    }
 
 }
 
@@ -33,27 +33,27 @@ void print1(void)
 void print2(void)
 {
 
-	for(int k = 0; k <= i; k++){
-		if(k == i)
-			printf("* ");
-		else
-			printf(" ");
-	}
-	
-	for(int j = 9; j > i; j--){
-		printf(" ");
-	}
+    for(int k = 0; k <= i; k++){
+        if(k == i)
+            printf("* ");
+        else
+            printf(" ");
+    }
+    
+    for(int j = 9; j > i; j--){
+        printf(" ");
+    }
 
-	for(int j = 9; j >= i; j--){
-		if(j == i)
-			printf("* ");
-		else
-			printf(" ");
-	}
+    for(int j = 9; j >= i; j--){
+        if(j == i)
+            printf("* ");
+        else
+            printf(" ");
+    }
 
-	for(int a = 0; a < i; a++){
-		printf(" ");
-	}
+    for(int a = 0; a < i; a++){
+        printf(" ");
+    }
 
 }
 
@@ -62,17 +62,17 @@ int main()
 {
 
 for(int l = 0; l < 5; l++){	
-	for(i = -1; i < 10; i++){
-		for(int x = 0; x < 6; x++)
-			print1();
-		printf("\n");
-	}
+    for(i = -1; i < 10; i++){
+        for(int x = 0; x < 6; x++)
+            print1();
+        printf("\n");
+    }
 
-	for(i = 0; i < 11; i++){
-		for(int x = 0; x < 6; x++)
-			print2();	
-		printf("\n");
-	}
+    for(i = 0; i < 11; i++){
+        for(int x = 0; x < 6; x++)
+            print2();	
+        printf("\n");
+    }
 }
 
 }
--- tiles_pattern2.c
+++ tiles_pattern2.c
@@ -4,59 +4,59 @@ int i = 0;
 
 void print1(void)
 {
-	for(int j = i; j < 10; j++){
-		printf(" ");
-	}
+    for(int j = i; j < 10; j++){
+        printf(" ");
+    }
 
-	for(int k = 0; k <= i; k++){
-		printf("*");
-	}
+    for(int k = 0; k <= i; k++){
+        printf("*");
+    }
 
-	for(int k = 0; k < i; k++){
-		printf("*");
-	}
+    for(int k = 0; k < i; k++){
+        printf("*");
+    }
 
-	for(int j = i; j < 10; j++){
-		printf(" ");
-	}
+    for(int j = i; j < 10; j++){
+        printf(" ");
+    }
 }
 
 void print2(void)
 {
-	for(int j = 1; j <= i; j++){
-		printf(" ");
-	}
+    for(int j = 1; j <= i; j++){
+        printf(" ");
+    }
 
-	for(int k = i; k < 10; k++){
-		printf("*");
-	}
+    for(int k = i; k < 10; k++){
+        printf("*");
+    }
 
-	for(int k = i; k < 11; k++){
-		printf("*");
-	}
+    for(int k = i; k < 11; k++){
+        printf("*");
+    }
 
-	for(int j = 0; j < i; j++){
-		printf(" ");
-	}
+    for(int j = 0; j < i; j++){
+        printf(" ");
+    }
 }
 
 int main()
 {
-	int x;
-	printf("Enter The Size of Pattern : ");
-	scanf("%d",&x);
+    int x;
+    printf("Enter The Size of Pattern : ");
+    scanf("%d",&x);
 
-	for(int l = 0; l < x; l++){
-		for(i = 0; i < 11; i++){	
-			for(int x = 0; x < 6; x++)
-				print1();
-			printf("\n");
-		}
+    for(int l = 0; l < x; l++){
+        for(i = 0; i < 11; i++){	
+            for(int x = 0; x < 6; x++)
+                print1();
+            printf("\n");
+        }
 
-		for(i = 1; i < 11; i++){	
-			for(int x = 0; x < 6; x++)
-				print2();
-			printf("\n");
-		}
-	}
+        for(i = 1; i < 11; i++){	
+            for(int x = 0; x < 6; x++)
+                print2();
+            printf("\n");
+        }
+    }
 }
